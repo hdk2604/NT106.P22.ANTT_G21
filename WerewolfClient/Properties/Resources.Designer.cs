1f4e3f8df783cd3d0ebe885b80ac280156d3b931
khieulam28 <khieubaolam28@gmail.com>
2025-05-03 17:37:28 +0700

    Them giao dien cho Roles

--- WerewolfClient/Properties/Resources.Designer.cs
+++ WerewolfClient/Properties/Resources.Designer.cs
@@ -73,9 +73,39 @@ namespace WerewolfClient.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Guardian {
+            get {
+                object obj = ResourceManager.GetObject("Guardian", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Hunter {
+            get {
+                object obj = ResourceManager.GetObject("Hunter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ProphetIcon {
             get {
                 object obj = ResourceManager.GetObject("ProphetIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Seer {
+            get {
+                object obj = ResourceManager.GetObject("Seer", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
